033e450b6e65eb709181319ec12180eb411846fe
Katy Nova <katy.novaaaa@yandex.ru>
2025-06-10 10:15:54 +0300

    Реализация поиска популярных фильмов по году и жанру

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateDbApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateDbApplicationTests.java
@@ -14,7 +14,6 @@ import ru.yandex.practicum.filmorate.dal.mapper.GenreRowMapper;
 import ru.yandex.practicum.filmorate.dal.mapper.MpaRowMapper;
 import ru.yandex.practicum.filmorate.dal.mapper.UserRowMapper;
 import ru.yandex.practicum.filmorate.dto.FilmDto;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
